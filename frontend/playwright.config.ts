3a45b1ac45f3e6ddb507c4a2bc9c3733cdf09c04
Joonas Heikura <e2101191@edu.vamk.fi>
2025-01-27 15:45:16 +0200

    e2e tests testing

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -76,14 +76,12 @@ export default defineConfig({
   webServer: [
     {
       command: "cd ../backend && npm run start:test",
-      timeout: 120 * 1000,
       url: "http://localhost:3001",
       reuseExistingServer: !process.env.CI,
     },
     {
       command: "npm run dev",
-      timeout: 120 * 1000,
-      url: "http://localhost:5173", // Frontend server URL
+      url: "http://localhost:5173",
       reuseExistingServer: !process.env.CI,
     },
   ],
