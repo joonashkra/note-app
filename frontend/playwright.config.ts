14c5c89a45a0fde81716d0500bae614209e3a1e7
Joonas Heikura <e2101191@edu.vamk.fi>
2025-01-27 16:43:19 +0200

    e2e tests testing

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:5173",
+    baseURL: "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -76,13 +76,13 @@ export default defineConfig({
   webServer: [
     {
       command: "npm run dev",
-      url: "http://10.1.0.6:5173",
+      url: "http://localhost:5173",
       timeout: 2 * 60 * 1000,
       reuseExistingServer: !process.env.CI,
     },
     {
       command: "npm run backend:pw",
-      url: "http://10.1.0.6:3001",
+      url: "http://localhost:3001",
       timeout: 2 * 60 * 1000,
       reuseExistingServer: !process.env.CI,
     },
